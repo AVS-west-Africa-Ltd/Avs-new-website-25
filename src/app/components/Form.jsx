d9e0c2d0af686d8bcd6dc30b8a0127e59a897417
kadrikondi <abdulkadri42@gmail.com>
2024-09-23 07:01:45 +0100

    update and correct build error

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,21 +1,21 @@
 "use client";
-import { useState } from 'react';
-import { useFileUpload } from '../libs/useFileUpload/route';
+import { useState } from "react";
+import { useFileUpload } from "../libs/useFileUpload/useFileUpload";
 const Form = () => {
   const [formData, setFormData] = useState({
-    Name: '',
-    LastName: '',
-    Email: '',
-    Phone: '',
-    LookingFor: '',
-    Comments: '',
-    Attachments: null
+    Name: "",
+    LastName: "",
+    Email: "",
+    Phone: "",
+    LookingFor: "",
+    Comments: "",
+    Attachments: null,
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
 
-    if (e.target.type === 'file') {
+    if (e.target.type === "file") {
       const file = e.target.files[0];
 
       // Create a temporary URL for the file
@@ -23,7 +23,7 @@ const Form = () => {
 
       setFormData((prevData) => ({
         ...prevData,
-        Attachments: file
+        Attachments: file,
       }));
     } else {
       setFormData((prevData) => ({
@@ -33,9 +33,6 @@ const Form = () => {
     }
   };
 
-
-
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(JSON.stringify(formData), "the form data");
@@ -44,7 +41,7 @@ const Form = () => {
     //   const uploadFile = useFileUpload();
     // const handleFileSelect = async (file) => {
     //   const uploadOk = await uploadFile(formData.Attachments.file.name, formData.Attachments.file)
-      
+
     //   if (uploadOk) {
     //     /// should return the file url, file name and file size
     //   } else {
@@ -73,44 +70,44 @@ const Form = () => {
       const jsonData = JSON.stringify(formData);
 
       // Make the API request to create a record
-      const response = await fetch('/api/airtable', {
-        method: 'POST',
+      const response = await fetch("/api/airtable", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: jsonData,
       });
 
       // Log the raw response
-      console.log('Raw Response:', await response.text());
+      console.log("Raw Response:", await response.text());
 
       if (response.ok) {
-        console.log('Record created successfully!');
+        console.log("Record created successfully!");
         setFormData({
-          Name: '',
-          LastName: '',
-          Email: '',
-          Phone: '',
-          LookingFor: '',
-          Comments: '',
+          Name: "",
+          LastName: "",
+          Email: "",
+          Phone: "",
+          LookingFor: "",
+          Comments: "",
           Attachments: null,
         });
       } else {
-        console.error('Failed to create record!!!');
+        console.error("Failed to create record!!!");
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
-  
-
-  
   return (
     <div className="min-h-screen flex items-center  text-white">
-      <form onSubmit={handleSubmit} className=" px-8 pt-6 pb-8 mb-4 grid grid-cols-2 grid-rows-6 gap-x-2 md:gap-x-4">
+      <form
+        onSubmit={handleSubmit}
+        className=" px-8 pt-6 pb-8 mb-4 grid grid-cols-2 grid-rows-6 gap-x-2 md:gap-x-4"
+      >
         <div className="mb-4">
-          <label className={` text-sm  mb-2 ${formData.Name && 'active'}`}>
+          <label className={` text-sm  mb-2 ${formData.Name && "active"}`}>
             First Name
             <input
               className="appearance-none border-b-[1px] border-white bg-transparent  w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
@@ -125,7 +122,7 @@ const Form = () => {
         </div>
 
         <div className="mb-4">
-          <label className={` text-sm  mb-2 ${formData.LastName && 'active'}`}>
+          <label className={` text-sm  mb-2 ${formData.LastName && "active"}`}>
             Last Name:
             <input
               className="appearance-none border-b-[1px] border-white bg-transparent  w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
@@ -140,7 +137,7 @@ const Form = () => {
         </div>
 
         <div className="mb-4">
-          <label className={` text-sm  mb-2 ${formData.Email && 'active'}`}>
+          <label className={` text-sm  mb-2 ${formData.Email && "active"}`}>
             Email:
             <input
               className="appearance-none border-b-[1px] border-white bg-transparent  w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
@@ -155,7 +152,7 @@ const Form = () => {
         </div>
 
         <div className="mb-4">
-          <label className={` text-sm  mb-2 ${formData.Phone && 'active'}`}>
+          <label className={` text-sm  mb-2 ${formData.Phone && "active"}`}>
             Phone Number:
             <input
               className="appearance-none border-b-[1px] border-white bg-transparent  w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
@@ -170,30 +167,33 @@ const Form = () => {
         </div>
 
         <div className="mb-4 col-span-2 relative">
-            <label className={`text-sm mb-2 ${formData.lookingFor && 'active'}`}>
-                Looking For:
-                <div className="relative">
-                <select
-                    className="appearance-none border-b-[1px] border-white bg-transparent w-full py-2 px-3 pr-8 leading-tight focus:outline-none focus:shadow-outline"
-                    name="LookingFor"
-                    value={formData.LookingFor}
-                    onChange={handleChange}
-                 
-                >
-                    <option value="">Select an option</option>
-                    <option value="Partnerships & Advisory">Partnerships & Advisory</option>
-                    <option value="Technical Activation">Technical Activation</option>
-                    <option value="Digital Activation">Digital Activation</option>
-                </select>
-                <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
-                    &darr;
-                </div>
-                </div>
-            </label>
+          <label className={`text-sm mb-2 ${formData.lookingFor && "active"}`}>
+            Looking For:
+            <div className="relative">
+              <select
+                className="appearance-none border-b-[1px] border-white bg-transparent w-full py-2 px-3 pr-8 leading-tight focus:outline-none focus:shadow-outline"
+                name="LookingFor"
+                value={formData.LookingFor}
+                onChange={handleChange}
+              >
+                <option value="">Select an option</option>
+                <option value="Partnerships & Advisory">
+                  Partnerships & Advisory
+                </option>
+                <option value="Technical Activation">
+                  Technical Activation
+                </option>
+                <option value="Digital Activation">Digital Activation</option>
+              </select>
+              <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                &darr;
+              </div>
+            </div>
+          </label>
         </div>
 
         <div className="mb-4 col-span-2 row-start-4">
-          <label className={`text-sm  mb-2 ${formData.Comments && 'active'}`}>
+          <label className={`text-sm  mb-2 ${formData.Comments && "active"}`}>
             Comment:
             <textarea
               className=" resize-none  appearance-none border-b-[1px] border-white bg-transparent  w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
@@ -201,10 +201,9 @@ const Form = () => {
               value={formData.Comments}
               onChange={handleChange}
             />
-
           </label>
         </div>
-{/* 
+        {/* 
         <div className="mb-4 col-span-2 row-start-5">
           <label className="text-sm  mb-2">
             Attach Your Brief
@@ -220,7 +219,10 @@ const Form = () => {
         </div> */}
 
         <div className=" justify-between col-span-2 row-start-6">
-            <p className='text-xs py-4'>By clicking the submit button you agree to our Terms of Use and Privacy Policy.</p>
+          <p className="text-xs py-4">
+            By clicking the submit button you agree to our Terms of Use and
+            Privacy Policy.
+          </p>
           <button
             className="bg-white text-black hover:bg-black hover:text-white transition duration-300  py-2 px-4 focus:outline-none focus:shadow-outline"
             type="submit"
