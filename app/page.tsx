928bd76ee9459c25799cdff858adce1e7aa16417
Elijah Obominuru <dev_team@plantec.io>
2025-03-29 18:48:58 +0100

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,6 @@ export default function Home() {
         <PartnershipSection />
         <FooterSection />
         <FeaturesSection />
-        <CallToActionSection />
       </div>
     </div>
   );
