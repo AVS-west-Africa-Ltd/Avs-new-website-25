f794caa13b62e927bb2a4e814d325b571abccad3
Elijah Obominuru <dev_team@plantec.io>
2025-03-30 20:29:43 +0100

    updates

--- app/contact-us/partials/ContactHeader.tsx
+++ app/contact-us/partials/ContactHeader.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+"use client";
+import React from "react";
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 
 const ContactHeader = () => {
   return (
-    <div className="bg-[#131211] text-white py-16 px-4 md:py-24 flex flex-col items-center justify-center text-center">
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+      className="bg-[#131211] text-white py-16 px-4 md:py-24 flex flex-col items-center justify-center text-center"
+    >
       <p className="text-lg mb-3">Contact Us</p>
-      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold max-w-3xl mb-8">
-        Share your project brief and ideas. Let's make it happen
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold max-w-4xl mb-8 mt-3 leading-snug">
+        Share your project brief and ideas. <br className="hidden md:block" /> Let's make it happen.
       </h1>
-      <Button variant="outline" className="bg-white !rounded-[4px] text-black hover:bg-gray-100 hover:text-black">
+      <Button
+        variant="outline"
+        className="bg-white !rounded-md text-black hover:bg-gray-200 hover:text-black transition-all duration-300"
+      >
         Let's connect now!
       </Button>
-    </div>
+    </motion.div>
   );
 };
 
