7f228e932676b484f2315ca2788d651c465d10c5
kadrikondi <abdulkadri42@gmail.com>
2025-04-22 17:46:55 +0100

    update seo text

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const raleway = Raleway({
 
 export const metadata: Metadata = {
   title: "A Venture Studio | AI-powered Tech and Digital Solutions",
-  description: "A Venture Studio is a venture studio that builds and invests in tech and digital driven solutions. We are dedicated to creating innovative products and services that leverage the power of artificial intelligence to solve real-world problems.",
+  description: "A Venture Studio is a venture studio that builds and invests in tech and digital driven solutions. We are dedicated to creating innovative products and services that leverage the power of expert humans and artificial intelligence to solve real-world problems.",
   icons: {
     icon: '/assets/Dark.png', // This references your icon file in the public folder
     apple: '/assets/Dark.png',
