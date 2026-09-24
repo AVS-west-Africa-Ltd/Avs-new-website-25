2161b5283b1ed3623a997f8974f86482b1d0ae7c
Joe-EtubiGreatattai <greatattai442442@gmail.com>
2026-09-24 10:02:23 +0100

    update

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"tZhh3F+V7IFRf7zpPPHmaYWYzuHUvhWvJ46XRmlfWFw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
